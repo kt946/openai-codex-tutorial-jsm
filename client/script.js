6b785e0814ccb062a3ade308b61a0f2007c08021
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-04 00:13:44 -0500

    added comments and updated script

--- client/script.js
+++ client/script.js
@@ -10,14 +10,19 @@ function loader(element) {
   element.textContent = '';
 
   loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
     element.textContent += '.';
 
+    // If the loading indicator has reached three dots, reset it
     if (element.textContent === '....') {
       element.textContent = '';
     }
   }, 300);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function typeText(element, text) {
   let index = 0;
 
@@ -71,15 +76,17 @@ const handleSubmit = async (e) => {
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
+  // to focus scroll to the bottom 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+  // specific message div 
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-p43u.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
@@ -94,7 +101,7 @@ const handleSubmit = async (e) => {
 
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
     typeText(messageDiv, parsedData);
   } else {
